e8eb71646e10cf4b43a22ebfe1be3743ef6efbd0
Joshua Sankarlal <joshua.sankarlal@uoit.net>
2019-01-26 18:19:18 -0500

    Level design, background part 1. Car moves

---
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Use this for initialization
+    public float moveSpeed;
+    //private CharacterController _controller;
+
+
+    void Start () {
+       // _controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + (new Vector3(0, moveSpeed,0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + (new Vector3(0,  - moveSpeed, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (new Vector3(-moveSpeed, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + (new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+        }
+
+    }
+}
---
+++ GlobalGameJam2019/Assets/Scripts/PlayerControllerPhysics.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPhysics : MonoBehaviour {
+
+   private Rigidbody2D playerMovement;
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+        playerMovement = GetComponent<Rigidbody2D>();
+        playerMovement.velocity = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerMovement.velocity = new Vector2(0,moveSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerMovement.velocity = new Vector2(0, - moveSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerMovement.velocity = new Vector2(-moveSpeed, 0 );
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerMovement.velocity = new Vector2(moveSpeed,0);
+        }
+
+    }
+}
