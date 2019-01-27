1d672c1374247a4181404778f3829698e47d9db7
Joshua Sankarlal <joshua.sankarlal@uoit.net>
2019-01-26 22:54:23 -0500

    Some assets and Emory fixed my shit dude cause it was SCREWED LMAO

--- GlobalGameJam2019/Assets/Scripts/PlayerController.cs
+++ GlobalGameJam2019/Assets/Scripts/PlayerController.cs
@@ -7,88 +7,109 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     public float moveSpeed;
     int currentLane;
-    bool lerping = false;
-  
+    bool lerping = true;
+    private float movementCounter;
     //private CharacterController _controller;
 
 
     void Start()
     {
-        currentLane = 1;
-
+        currentLane = 2;
+        movementCounter = 0;
         // _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //Debug.Log("Lerping is currently: " + lerping);
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = transform.position + (new Vector3(0, moveSpeed, 0) * Time.deltaTime);
+             transform.position += (new Vector3(0, moveSpeed, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position = transform.position + (new Vector3(0, -moveSpeed, 0) * Time.deltaTime);
+           transform.position += (new Vector3(0, -moveSpeed, 0) * Time.deltaTime);
         }
 
         if (Input.GetKeyUp(KeyCode.A))
         {
-            if (currentLane == 1 || currentLane == 3)
+            currentLane--;
+            if (currentLane >= 0)
             {
-                lerping = true;
+                movementCounter = 0;
             }
+            else
+            {
+                currentLane++;
+            }
+            //if (currentLane == 1 || currentLane == 3)
+            //{
+            //    lerping = true;
+            //}
         }
 
         if (Input.GetKeyUp(KeyCode.D))
         {
-            if (currentLane == 1 || currentLane == 2)
+            currentLane++;
+            if (currentLane <= 3)
             {
-                lerping = true;
+                movementCounter = 0;
             }
+            else
+            {
+                currentLane--;
+            }
+            //if (currentLane == 1 || currentLane == 2)
+            //{
+            //    lerping = true;
+            //}
         }
 
-        if (lerping)
+        movementCounter += Time.deltaTime * moveSpeed;
+        if (movementCounter > 1)
         {
-
+            movementCounter = 1;
+        }
             if (currentLane == 1)
             {
-                
-                transform.position = Vector2.Lerp(transform.position, new Vector2(-1.80f, transform.position.y), moveSpeed * Time.deltaTime); //Move to Lane 2 
+
+                transform.position = Vector2.Lerp(transform.position, new Vector2(-1.80f, transform.position.y), movementCounter); //Move to Lane 2 
             }
 
-                     
+
             if (currentLane == 2)
             {
-                transform.position = Vector2.Lerp(transform.position, new Vector2(-0.011f, transform.position.y), moveSpeed * Time.deltaTime);
+                transform.position = Vector2.Lerp(transform.position, new Vector2(-0.011f, transform.position.y), movementCounter );
 
             }
 
             if (currentLane == 3)
             {
-                transform.position = Vector2.Lerp(transform.position, new Vector2(1.90f, transform.position.y), moveSpeed * Time.deltaTime);
+                transform.position = Vector2.Lerp(transform.position, new Vector2(1.90f, transform.position.y), movementCounter );
             }
 
-            if (transform.position.x == -1.80f) //If in Lane 2
-            {
-                lerping = false;
-                currentLane = 2;
-            }
+            //if (transform.position.x == -1.80f) //If in Lane 2
+            //{
+            //    lerping = false;
+            //    currentLane = 2;
+            //}
+            //
+            //if (transform.position.x == -0.011f) //If in Lane 1
+            //{
+            //    lerping = false;
+            //    currentLane = 1;
+            //}
+            //
+            //if (transform.position.x == 1.90f)
+            //{
+            //    lerping = false;
+            //    currentLane = 3;
+            //}
 
-            if (transform.position.x == -0.011f) //If in Lane 1
-            {
-                lerping = false;
-                currentLane = 1;
-            }
-
-            if (transform.position.x == 1.90f)
-            {
-                lerping = false;
-                currentLane = 3;
-            }
-
-        }
+        
     }
 
 
