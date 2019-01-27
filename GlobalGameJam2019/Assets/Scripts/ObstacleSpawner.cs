5bf4ee3834c10383a68125aa2a81173360859835
Joshua Sankarlal <joshua.sankarlal@uoit.net>
2019-01-27 15:43:13 -0500

    Menu

--- GlobalGameJam2019/Assets/Scripts/ObstacleSpawner.cs
+++ GlobalGameJam2019/Assets/Scripts/ObstacleSpawner.cs
@@ -7,17 +7,37 @@ public class ObstacleSpawner : MonoBehaviour {
     public GameObject obstacle;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
+    float difficultyIncreaseTime=5;
+    float bigTimer;
+    float currentSpawnTime;
 	// Use this for initialization
-	void Start ()
-    {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-	}
+
 	
-	// Update is called once per frame
+    private void Update()
+    {
+        currentSpawnTime += Time.deltaTime;
+        bigTimer += Time.deltaTime;
+
+        if (currentSpawnTime>=spawnTime)
+        {
+            Spawn();
+            currentSpawnTime = 0;
+        }
+
+        if (bigTimer>=difficultyIncreaseTime)
+        {
+            spawnTime -= .5f;
+            difficultyIncreaseTime -= 50;
+            bigTimer = 0;
+        }
+    }
+	
+    // Update is called once per frame
 	void Spawn()
     {
-
+        Debug.Log("Spawn Time: " + spawnTime);
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
         Instantiate(obstacle, spawnPoints[spawnPointIndex].position,spawnPoints[spawnPointIndex].rotation);
     }
+
 }
